313b049669866910142f1dc8c1fa50757f67b7dd
ASUBRIX <asubrix@gmail.com>
2025-05-28 21:46:40 +0530

    API health check

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.get('/health', (req, res) => res.sendStatus(200));
 
 // Public/User Routes
 app.use('/api', userHomeRoutes);
