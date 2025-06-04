8a21c24f714f782389b603dc2b04e5be8d574399
ASUBRIX <asubrix@gmail.com>
2025-06-04 18:05:56 +0530

    Fix:Not restrict Enquiry Form before login

--- routes/user/enquiry.js
+++ routes/user/enquiry.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const enquiryController = require('../../controllers/user/enquiryController');
-const {auth} = require("../../middleware/auth");
 
-router.use(auth);
 
-// Submit new enquiry
 router.post('/', enquiryController.createEnquiry);
 
 module.exports = router;
